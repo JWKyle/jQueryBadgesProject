d4e3db103c7efc92b8b793316e4e5db48ac06391
Jim Kyle <jim@jwkyle.com>
2017-12-26 11:39:21 -0600

    add in progress section, add jquery to show all badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,12 +4,40 @@ $(function() {
   url: 'https://www.codeschool.com/users/jwkyle.json',
   dataType: 'jsonp',
   success: function(response) {
-    // console.log('Success!', response.courses.completed);
+    console.log('Success!', response.courses.in_progress);
     completedCourses(response.courses.completed);
+    progressCourses(response.courses.in_progress);
   }
 });
 
 function completedCourses(courses) {
+
+  var $badges = $('#badges');
+
+  courses.forEach(function(course) {
+    // console.log(course);
+
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />', {
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See Course'
+    }).appendTo($course);
+  })
+}
+function progressCourses(courses) {
 
   var $badges = $('#badges');
 
